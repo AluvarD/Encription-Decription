d683b7cdba161e37e42c67d216fe5a47a4550f64
AluvarD <antipov_ilya26@mail.ru>
2021-01-11 19:55:51 +0300

    stage 2 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,48 +5,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //String input = scanner.nextLine();
-        String input = "we found a treasure!";
-        String output = encrypt(input);
+        String input = scanner.nextLine();
+        int key = scanner.nextInt();
+        //String input = "we found a treasure!";
+        String output = encrypt(input, key);
         System.out.println(output);
     }
 
-    //first version
-    /*static String encrypt (String input) {
+    static String encrypt(String input, int key) {
         StringBuilder encrypted = new StringBuilder();
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        String alphabetRevers = "zyxwvutsrqponmlkjihgfedcba";
-        String[] inputArray = input.split(" ");
-        char temp;
-        int alphabetIndex;
-
-        for (String s : inputArray) {
-            for (int j = 0; j < s.length(); j++) {
-                temp = s.charAt(j);
-                alphabetIndex = alphabet.indexOf(temp);
-                if (alphabetIndex >= 0) {
-                    encrypted.append(alphabetRevers.charAt(alphabetIndex));
-                } else {
-                    encrypted.append(s.charAt(j));
-                }
-            }
-            encrypted.append(" ");
-        }
-
-        return String.valueOf(encrypted);
-    }*/
-
-    static String encrypt (String input) {
-        StringBuilder encrypted = new StringBuilder();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        String alphabetRevers = "zyxwvutsrqponmlkjihgfedcba";
         char temp;
         int alphabetIndex;
         for (int j = 0; j < input.length(); j++) {
             temp = input.charAt(j);
             alphabetIndex = alphabet.indexOf(temp);
-            if (alphabetIndex >= 0) {
-                encrypted.append(alphabetRevers.charAt(alphabetIndex));
+
+            if (alphabetIndex >= 0 && alphabetIndex + key < 25) {
+                encrypted.append(alphabet.charAt(alphabetIndex + key));
+            } else if (alphabetIndex + key > 25) {
+                alphabetIndex = alphabetIndex - 26;
+                encrypted.append(alphabet.charAt(alphabetIndex + key));
             } else {
                 encrypted.append(input.charAt(j));
             }
@@ -54,4 +33,5 @@ public class Main {
 
         return String.valueOf(encrypted);
     }
+
 }
