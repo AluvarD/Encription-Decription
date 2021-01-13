a18b60b4b117fd229ccb18dcfe77776635f35b1a
AluvarD <antipov_ilya26@mail.ru>
2021-01-13 19:06:02 +0300

    stage 3 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,31 +4,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        encryptionDecryption();
+    }
+
+    static void encryptionDecryption() {
         Scanner scanner = new Scanner(System.in);
+        String mode = scanner.nextLine();
         String input = scanner.nextLine();
         int key = scanner.nextInt();
-        //String input = "we found a treasure!";
-        String output = encrypt(input, key);
+        String output;
+
+        switch (mode) {
+            case "enc":
+                output = encrypt(input, key);
+                break;
+            case "dec":
+                output = decrypt(input, key);
+                break;
+            default:
+                output = "error";
+                break;
+        }
+
         System.out.println(output);
     }
 
     static String encrypt(String input, int key) {
         StringBuilder encrypted = new StringBuilder();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        char temp;
-        int alphabetIndex;
+        int temp;
         for (int j = 0; j < input.length(); j++) {
             temp = input.charAt(j);
-            alphabetIndex = alphabet.indexOf(temp);
+            temp += key;
+            encrypted.append((char) temp);
+        }
 
-            if (alphabetIndex >= 0 && alphabetIndex + key < 25) {
-                encrypted.append(alphabet.charAt(alphabetIndex + key));
-            } else if (alphabetIndex + key > 25) {
-                alphabetIndex = alphabetIndex - 26;
-                encrypted.append(alphabet.charAt(alphabetIndex + key));
-            } else {
-                encrypted.append(input.charAt(j));
-            }
+        return String.valueOf(encrypted);
+    }
+
+    static String decrypt(String input, int key) {
+        StringBuilder encrypted = new StringBuilder();
+        int temp;
+        for (int j = 0; j < input.length(); j++) {
+            temp = input.charAt(j);
+            temp -= key;
+            encrypted.append((char) temp);
         }
 
         return String.valueOf(encrypted);
